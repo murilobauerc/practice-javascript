c947e7e11bb90a0105b6ce348c67797a35d339e0
Murilo Rosa <murilobauerc@gmail.com>
2019-03-08 09:25:19 -0300

    :docs:

---
+++ exercicios-js/fundamentos/objeto.js
@@ -0,0 +1,15 @@
+// objeto é uma coleção de chave e valor
+
+const prod1 = {};
+prod1.nome = "Celular Ultra Mega";
+prod1.preco = 4998.9;
+prod1["desconto Legal"] = 0.4; // evitar atributos com espaço
+
+console.log(prod1);
+
+const prod2 = {
+  nome: "Camisa Polo",
+  preco: 79.9
+};
+
+console.log(prod2);
