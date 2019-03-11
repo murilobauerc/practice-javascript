b2aed734dd7f65a4b293a0b8534a0929aea58855
Murilo Rosa <murilobauerc@gmail.com>
2019-03-11 14:36:10 -0300

    :memo:

--- exercicios-js/fundamentos/destructuring1.js
+++ exercicios-js/fundamentos/destructuring1.js
@@ -1,7 +1,19 @@
 // novo recurso do ES2015
- 
-const pessoa = { 
-    nome: 'Ana',
-    idade: 5,
-    endereco:
-}
+
+const pessoa = {
+  nome: "Ana",
+  idade: 5,
+  endereco: {
+    logradouro: "RUA Abc",
+    numero: 1000
+  }
+};
+
+const { nome, idade } = pessoa;
+console.log(nome, idade);
+
+const { nome: n, idade: i } = pessoa;
+console.log(n, i);
+
+const { sobrenome, bemHumorada = true } = pessoa;
+console.log(sobrenome, bemHumorada);
