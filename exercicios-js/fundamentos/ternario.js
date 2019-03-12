c70b3aa4bd4385e993b9ab994b7a1d7948a1de77
Murilo Rosa <murilobauerc@gmail.com>
2019-03-12 08:53:42 -0300

    :art: we passed through javascript fundamentals

---
+++ exercicios-js/fundamentos/ternario.js
@@ -0,0 +1,9 @@
+const resultado = nota => (nota >= 7 ? "Aprovado" : "Reprovado");
+
+console.log(resultado(7.1));
+console.log(resultado(6.7));
+
+// operador ternario CONTÉM 3 partes
+// primeira parte: operador relacional
+// segunda parte: 'aprovado'
+// terceira parte: 'reprovado'
