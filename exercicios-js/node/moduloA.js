ff9cfd7296c955f91f91cc99a724522725839941
murilobauerc <panchobauer@gmail.com>
2019-06-24 16:35:21 -0300

    Understanding of architecture of node

---
+++ exercicios-js/node/moduloA.js
@@ -0,0 +1,7 @@
+// um módulo é um arquivo
+// módulo tem interioridade, fica vísivel apenas no módulo
+console.log(this);
+
+this.ola = "Fala pessoal";
+exports.bemVindo = "Bem vindo ao node!";
+module.exports.ateLogo = "Até proximo exemplo";
