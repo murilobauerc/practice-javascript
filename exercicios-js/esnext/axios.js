250aa77f40c820adb9596dce45bacf0de7679d97
murilobauerc <panchobauer@gmail.com>
2019-09-11 17:01:35 -0300

    functions promise based

---
+++ exercicios-js/esnext/axios.js
@@ -0,0 +1,16 @@
+const axios = require("axios");
+
+const URL_TURMA_A = "http://files.cod3r.com.br/curso-js/turmaA.json";
+const URL_TURMA_B = "http://files.cod3r.com.br/curso-js/turmaB.json";
+
+// Promise example
+// function getFirstUser() {
+//     return getUsers().then(function(users) {
+//         return users[0].name;
+//     });
+// }
+
+// async function getFirstUser() {
+//     let users = await getUsers();
+//     return users[0].name;
+// }
