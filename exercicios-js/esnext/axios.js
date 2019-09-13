de3ac4ee0866f93b847d75fe02939312be8233e9
murilobauerc <panchobauer@gmail.com>
2019-09-13 14:32:36 -0300

    more samples with asynchronous functions

--- exercicios-js/esnext/axios.js
+++ exercicios-js/esnext/axios.js
@@ -3,6 +3,18 @@ const axios = require("axios");
 const URL_TURMA_A = "http://files.cod3r.com.br/curso-js/turmaA.json";
 const URL_TURMA_B = "http://files.cod3r.com.br/curso-js/turmaB.json";
 
+// https://jsonplaceholder.typicode.com/posts
+
+const fetchApi = () => {
+  axios
+    .get("https://jsonplaceholder.typicode.com/posts")
+    .then(response => console.log(response.data));
+};
+
+fetchApi();
+
+console.log("Tchau"); // 3
+
 // Promise example
 // function getFirstUser() {
 //     return getUsers().then(function(users) {
